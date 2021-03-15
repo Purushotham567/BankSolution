178181cd49ec45af08a98c78db2d888ce7e27f42
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-03-15 17:03:54 +0530

    Procedurial

--- BankConApp/Program.cs
+++ BankConApp/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
+using BankLibrary;
+
 namespace BankConApp
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to Bank App: " + DateTime.Now);
+      //Console.WriteLine("Welcome to Bank App: " + DateTime.Now);
+
+      Account account = new Account();
+
+      Console.Write("Enter a AccountNumber: ");
+      int accountNumber = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Enter a HoldersName: ");
+      string holdersName = Console.ReadLine();
+
+      Console.Write("Enter a AccountType: ");
+      string accountType = Console.ReadLine();
+
+      Console.Write("Enter a Balance: ");
+      decimal balance = Convert.ToDecimal(Console.ReadLine());
+
+      account.AcceptDetails(accountNumber, holdersName, balance, accountType);
+      account.DisplayDetails();
     }
   }
 }
--- BankLibrary/Account.cs
+++ BankLibrary/Account.cs
@@ -4,6 +4,23 @@ namespace BankLibrary
 {
   public class Account
   {
+    int _accountNumber;
+    string _holdersName;
+    decimal _balance;
+    string _accountType;
 
+    public void AcceptDetails(int accountNumber, string holdersName, decimal balance, string accountType)
+    {
+      _accountNumber = accountNumber;
+      _holdersName = holdersName;
+      _balance = balance;
+      _accountType = accountType;
+    }
+
+    public void DisplayDetails()
+    {
+      Console.WriteLine("AccountNumber: " + _accountNumber + ", HoldersName: " + _holdersName);
+      Console.WriteLine("Balance: {0} and AccountType: {1}", _balance, _accountType);
+    }
   }
 }
