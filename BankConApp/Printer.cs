07bec1c2b5806a3086bf390667d14970bdeab37e
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-03-16 14:33:16 +0530

    Static class and method

---
+++ BankConApp/Printer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConApp
+{
+  public static class Printer
+  {
+    public static void Print(string message, string header = null, string pattern = "-", int noOfPattern = 100)
+    {
+      int headerLength = 0;
+      if (header != null)
+      {
+        headerLength = header.Length;
+      }
+      for (int i = 0; i < (noOfPattern-headerLength); i++)
+      {
+        Console.Write(pattern);
+        if (i == ((noOfPattern-headerLength)/ 2))
+        {
+          Console.Write(header);
+        }
+      }
+      Console.WriteLine($"\n{message}");
+      for (int i = 0; i < noOfPattern; i++)
+      {
+        Console.Write(pattern);
+      }
+      Console.WriteLine();
+    }
+  }
+}
--- BankConApp/Program.cs
+++ BankConApp/Program.cs
@@ -16,15 +16,18 @@ namespace BankConApp
       //acc = new CurrentAccount(202, "Snowy", 7000.00m);
       //acc = new FixedDepositAccount(301, "Thomson", 2000.00m);
 
-      Console.WriteLine(acc);
+      //Console.WriteLine(acc);
+
+      Printer.Print(message: acc.ToString(), header: "Account Details");
+
       acc.Deposit(100);
       Console.WriteLine($"New Balance: {acc.Balance}, After 100 Deposit");
       acc.Withdraw(100);
       Console.WriteLine($"New Balance: {acc.Balance}, After 100 Withdrawn");
-      Console.ReadKey(true);
+      //Console.ReadKey(true);
       //acc.Withdraw(7000.90m);
       //Console.WriteLine($"New Balance: {acc.Balance}, After 7000 Withdrawn");
-      Console.WriteLine(acc[0]);
+      Printer.Print(message: acc[0].ToString(), header: "Account Number");
       Console.WriteLine(acc[1]);
       Console.WriteLine(acc[2]);
       Console.WriteLine(acc[3]);
